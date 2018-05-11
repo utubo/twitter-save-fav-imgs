bb6d539f9e6c134647b456a99e186a56300107b6
utubo <utbutb@gmail.com>
2018-05-11 22:57:39 +0900

    ちょっと整理

--- save_fav_imgs.rb
+++ save_fav_imgs.rb
@@ -24,25 +24,22 @@ File.open("#{__dir__}/save_fav_imgs.json") do |f|
 		OAuth::Consumer.new(
 			ini['oauth']['consumer_key'],
 			ini['oauth']['consumer_key_secret'],
-			{:site=>'https://api.twitter.com'}),
+			{:site=>'https://api.twitter.com'}
+		),
 		ini['oauth']['access_token'],
 		ini['oauth']['access_token_secret']
 	)
+	$USER_AGENT = 'save_fav_imgs'
 end
 
 $TWITTER_TRY_COUNT = 2
 # API実行
-BOUNDARY = "--bboounnddaallyy"
-def twitter_execute(uri, post_body = nil, limit = 10)
+def twitter_execute(uri, limit = 10)
 	0.upto($TWITTER_TRY_COUNT) { |try_count|
 		begin
 			res = ""
 			Timeout.timeout(60) {
-				if post_body then
-					res = $ACCESS_TOKEN.post(uri, post_body)
-				else
-					res = $ACCESS_TOKEN.get(uri, {'User-Agent' => "#{$USER_AGENT}"})
-				end
+				res = $ACCESS_TOKEN.get(uri, {'User-Agent' => "#{$USER_AGENT}"})
 			}
 			case res
 			when Net::HTTPSuccess then
@@ -54,35 +51,31 @@ def twitter_execute(uri, post_body = nil, limit = 10)
 				if 0 < limit then
 					req.path = res['location']
 					Log.info("redirect to #{req.path}")
-					return twitter_execute(req, post_body, limit - 1)
+					return twitter_execute(req, limit - 1)
 				else
 					Log.warn("redirect over")
 					return nil
 				end
 			when Net::HTTPNotFound, Net::HTTPForbidden, Net::HTTPBadGateway then
-				Log.error("unretryable error. responce code:#{res.code} msg:#{res.message}")
+				Log.error("unretryable error. responce code #=> #{res.code} msg #=> #{res.message}")
 				Log.debug(res.body)
 				return nil
 			else
-				Log.warn("unknown responce code => #{res.code} msg => #{res.message} body => #{res.body}")
+				Log.warn("unknown responce code #=> #{res.code} msg #=> #{res.message}")
+				Log.debug(res.body)
 			end
 		rescue => e
-			Log.error(e) if try_count == $TWITTER_TRY_COUNT
-			Log.warn(e)
-		rescue Timeout::Error => e
-			Log.error(e) if try_count == $TWITTER_TRY_COUNT
-			Log.warn(e)
+			if try_count < $TWITTER_TRY_COUNT
+				Log.warn(e)
+				Log.info('sleep 3 sec... and Retry to execute Twitter-Api !')
+				sleep(3)
+			else
+				Log.error(e)
+				Log.error("Retry over !!!")
+			end
 		end
-
-		Log.info('sleep 3 sec... and Retry to execute Twitter-Api !')
-		sleep(3)
 	}
-	Log.error("Retry over !!!")
 	return nil
-end
-
-def get_api(api_url)
-	return twitter_execute(api_url)
 end
 
 def comp_str(a, b)
@@ -92,30 +85,33 @@ def comp_str(a, b)
 	return (a.size < b.size || a < b) ? -1 : 1
 end
 
-def add_json(src, since_id, max_id = nil)
+def save_images(json, since_id, max_id = nil)
 	last_id = since_id
 	first_id = max_id
-	Log.warn("src #=> nil") if src == nil || src.empty?
-	json_obj = JSON.parse(src)
+	Log.warn("json #=> nil") if json == nil || json.empty?
+	json_obj = JSON.parse(json)
 	json_obj.map { |status|
+		# 古いツイートをスキップする
 		id = status['id_str'];
 		break if comp_str(id, since_id) <= 0 && max_id.to_s.empty? == nil
 		last_id = id if comp_str(last_id, id) < 0
 		first_id = id if comp_str(id, first_id) < 0 || first_id.to_s.empty?
+		# ツイートの情報を取得する
 		Log.debug(id);
 		entities = status['extended_entities']
 		next if entities == nil
 		media = entities['media']
 		next if media == nil
+		# ツイートの情報からディレクトリ名とファイルパスを作成する
 		screen_name = status['user']['screen_name']
-		a = Time::parse(status['created_at'])
-		created_at = a.localtime.strftime('%Y%m%d_%H%M%S')
+		created_at = Time::parse(status['created_at']).localtime.strftime('%Y%m%d_%H%M%S')
 		text = status['text'].strip
 		escaped_text = text.gsub(/http.+/, '').gsub(/[\\\/:*?"<>|()]|\s/, '_').sub(/_+$/, '')
 		dir = "#{$SAVE_DIR}/@#{screen_name}"
 		file_name = "#{created_at}_#{escaped_text}"
 		file_name = file_name[0..47] + '...' if 50 < file_name.size
 		file_path = "#{dir}/#{file_name}"
+		# メディアファイルを保存する
 		Dir.mkdir(dir, 0755) if !Dir.exists?(dir)
 		Log.info("media.map.size => #{media.map.size}")
 		index = 0
@@ -123,7 +119,7 @@ def add_json(src, since_id, max_id = nil)
 		media.map { |m|
 			index += 1
 			video_info = m['video_info']
-			url = video_info == nil ? m['media_url'] : video_info['variants'][0]['url']
+			url = video_info == nil ? m['media_url'] : video_info['variants'][0]['url'] # 画像のURL or 動画のURL
 			ext = url.sub(/.+\//, '').sub(/.+\./, '').sub(/[\?#;].*/, '')
 			index_str = has_other_medias ? "_#{index}" : ''
 			file_path = "#{dir}/#{file_name}#{index_str}.#{ext}"
@@ -134,13 +130,14 @@ def add_json(src, since_id, max_id = nil)
 					local_file.write(remote_file.read)
 				}
 			}
-		#video_info = media['video_info']
-		#variants = video_info != nil ? media['variants'] : nil
 		}
 	}
 	return last_id, first_id
 end
 
+# ------------
+# START HERE !
+# ------------
 opts = OptionParser.new
 last_id = ''
 max_id = nil
@@ -161,14 +158,14 @@ end
 for i in 1..roop_count do
 	sleep(sleep_sec) if i != 1
 	url = $FAV_URL
-	url = url + '&since_id='+ last_id.to_s if !last_id.to_s.empty? && roop_count == 1
-	url = url + '&max_id='+ max_id.to_s if !max_id.to_s.empty? && roop_count != 1
+	url << '&since_id='+ last_id.to_s if !last_id.to_s.empty? && roop_count == 1
+	url << '&max_id='+ max_id.to_s if !max_id.to_s.empty? && roop_count != 1
 	Log.info("execute-api #{url}");
-	res = get_api(url);
-	last_id, max_id = add_json(res, last_id, max_id);
+	res = twitter_execute(url);
+	last_id, max_id = save_images(res, last_id, max_id);
 end
-if (roop_count == 1)
-	open("#{$LAST_ID_FILE}", 'w') {|f|
+if roop_count == 1
+	open("#{$LAST_ID_FILE}", 'w') { |f|
 		f.write(last_id)
 	}
 end
